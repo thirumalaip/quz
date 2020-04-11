0c04038a3d93a61637a1f0a0240847b96e05e1fe
thirumalaip <thirumalaitm205022@gmail.com>
2020-04-11 13:31:16 -0500

    build

---
+++ build/precache-manifest.3086839c3a2ae6f266ec7a2db5b35da1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b259da626c3c9e54098b189876dcb44b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a7fec38ccb9f912fe41",
+    "url": "/static/css/main.5eec0f90.chunk.css"
+  },
+  {
+    "revision": "29aa3fb60d55809ecb3a",
+    "url": "/static/js/2.4c5cbed7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4c5cbed7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a7fec38ccb9f912fe41",
+    "url": "/static/js/main.57b983ef.chunk.js"
+  },
+  {
+    "revision": "bedac97e389f5dcdfb0e",
+    "url": "/static/js/runtime-main.9c8d1b48.js"
+  }
+]);
